6991355b87abce030e2904a0ae2e8322cce2d7e2
Van <vandirection24@gmail.com>
2025-04-03 11:39:32 +0300

    Add learning_proj folder

---
+++ learning_proj/backend/app/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class _ServerId(BaseModel):
+    _id: int
+
+class ServerCreate(_ServerId, BaseModel):
+    hostname: str
+    type: str
+    cores: int
+    ram: int
+    disk: int
+class Server(BaseModel):
+    id: int
+    hostname: str
+    type: str
+    cores: int
+    ram: int
+    disk: int
+
+    class Config:
+        orm_mode = True
+        # from_attributes = True
